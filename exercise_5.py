8d79442f7572fbe6563dd1fefa5d30471cf42bcf
kingcrowley <kingcrowley@users.noreply.github.com>
2018-03-23 14:30:29 +0000

    Setting up exercise 5 file
    Starting exercise_5. Setting up py file with comments and goals

---
+++ exercise_5.py
@@ -0,0 +1,8 @@
+# David Crowley g00364706
+
+#Write a Python script that reads the Iris data set in and 
+# prints the four numerical values on each row in a nice format. 
+# That is, on the screen should be printed the petal length, petal width, sepal length and sepal width,
+#  and these values should have the decimal places aligned, with a space between the columns.
+
+#
