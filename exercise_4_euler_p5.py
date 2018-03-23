bb0951e82ea29e25a4f0c98c8e5ce883947c3c32
kingcrowley <kingcrowley@users.noreply.github.com>
2018-03-23 15:04:20 +0000

    Better comments
    Updated with better comments and references to code used and explanation used to understand coding.

--- exercise_4_euler_p5.py
+++ exercise_4_euler_p5.py
@@ -4,6 +4,9 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 # Smallest positive number that is evenly divisible is the Greatest Common Divisor (gcd)
+
+# Code from link below
+# https://stackoverflow.com/questions/37237954/calculate-the-lcm-of-a-list-of-given-numbers-in-python/42472824
 from fractions import gcd
 a = [1, 2, 3, 4, 5, 6, 7 , 8, 9 , 10,11, 12, 13, 14, 16, 17, 18, 19, 20]   #will work for an int array of any length
 lcm = a[0]
@@ -11,6 +14,10 @@ for i in a[1:]:
  # print(gcd(lcm, i))
  # finding the lcm of the current lcm and i by dividing by the gcd
  # current lcm * i / greatest common divisor of the lcm and i 
+
+ # if we know the greatest common divisor (GCD) of integers a and b, 
+ # we can calculate the LCM using the following formula. LCM(a,b) = (a*b)/GCD(a,b)
+ # i found this info at https://www.idomaths.com/hcflcm.php
   lcm = lcm*i/gcd(lcm, i)
  # print("Current lcm: "+ str(lcm))
 print("Overall :"+ str(lcm))
