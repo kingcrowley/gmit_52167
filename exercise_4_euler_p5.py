05314c108c89af47f61705004dbc8f476864b0f8
kingcrowley <kingcrowley@users.noreply.github.com>
2018-04-04 21:01:51 +0100

    Added in timing
    Added in timing of process and changed the print out to format the time better using print formatting

--- exercise_4_euler_p5.py
+++ exercise_4_euler_p5.py
@@ -7,10 +7,16 @@
 
 # Code from link below
 # https://stackoverflow.com/questions/37237954/calculate-the-lcm-of-a-list-of-given-numbers-in-python/42472824
+
+
+import time
 from fractions import gcd
 a = [1, 2, 3, 4, 5, 6, 7 , 8, 9 , 10,11, 12, 13, 14, 16, 17, 18, 19, 20]   #will work for an int array of any length
 lcm = a[0]
+# get current time (unix time)
+start = time.time()
 for i in a[1:]:
+
  # print(gcd(lcm, i))
  # finding the lcm of the current lcm and i by dividing by the gcd
  # current lcm * i / greatest common divisor of the lcm and i 
@@ -19,7 +25,9 @@ for i in a[1:]:
  # we can calculate the LCM using the following formula. LCM(a,b) = (a*b)/GCD(a,b)
  # found this info comes from at https://www.idomaths.com/hcflcm.php 
   lcm = lcm*i/gcd(lcm, i)
- # print("Current lcm: "+ str(lcm))
-print("Overall :"+ str(lcm))
-
-# answer is 232792560
+# used for debugging to comapre start time to current time
+#print(time.time())
+# timetaken = current time - the start time
+elapsed = (time.time() - start)
+print ("LCM = %s returned in %f seconds."% (lcm,elapsed))
+# answer is 232792560 - time take is normally too small for this simple calculation to see
